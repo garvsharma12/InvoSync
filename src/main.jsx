507bed5128088c503fe0fc8de31794995222cdf4
grv <garv.sharma200212@gmail.com>
2025-08-11 16:34:15 +0530

    Created MainPage adding basic Functionalities to it

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-import { StrictMode } from 'react'
+//import ReactDOM from 'react-dom/client'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
 import App from './App.jsx'
+import {AppContextProvider} from "./components/context/appContext.jsx";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <AppContextProvider>
     <App />
-  </StrictMode>,
+  </AppContextProvider>,
 )
